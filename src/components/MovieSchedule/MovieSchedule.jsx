c398b7acdcd8d6cfc7c5a6472092c770a82a7a78
Nhung <hongnhung16052003@gmail.com>
2024-10-23 12:44:45 +0700

    show images, movie schedule and genres of movie details

--- src/components/MovieSchedule/MovieSchedule.jsx
+++ src/components/MovieSchedule/MovieSchedule.jsx
@@ -1,70 +1,43 @@
-import React, { useState, useEffect } from "react";
+import React, {useState} from "react";
 
-const formatDate = (date) => {
-  const day = String(date.getDate()).padStart(2, "0");
-  const month = String(date.getMonth() + 1).padStart(2, "0");
-  return `${day}/${month}`;
-};
+const MovieSchedule = ({ data }) => {
+  const [selectedDate, setSelectedDate] = useState(data[0]);
 
-const getNext7Days = () => {
-  const dates = [];
-  const currentDate = new Date();
-  for (let i = 0; i < 7; i++) {
-    const date = new Date(currentDate);
-    date.setDate(currentDate.getDate() + i);
-    dates.push(formatDate(date));
+  function getTimeFromDateTime(dateTime) {
+    const timePart = dateTime.split("T")[1]; 
+    const [hour, minute] = timePart.split(":");
+    return `${hour}:${minute}`; 
   }
-  return dates;
-};
-
-const MovieSchedule = () => {
-  const [selectedDate, setSelectedDate] = useState(formatDate(new Date()));
-  const [dates, setDates] = useState([]);
-
-  useEffect(() => {
-    setDates(getNext7Days());
-  }, []);
-  const timeSlots = [
-    { time: "5:10", price: "65K" },
-    { time: "7:30", price: "65K" },
-    { time: "9:00", price: "65K" },
-    { time: "10:00", price: "65K" },
-    { time: "12:00", price: "65K" },
-    { time: "15:00", price: "65K" },
-    { time: "16:00", price: "65K" },
-    { time: "18:00", price: "65K" },
-    { time: "19:00", price: "65K" },
-    { time: "20:00", price: "90K" },
-    { time: "22:00", price: "120K" },
-    { time: "23:00", price: "65K" },
-    { time: "23:10", price: "65K" },
-  ];
-
+ 
   return (
     <div className="p-4">
       <div className="flex justify-center space-x-4 overflow-x-auto mb-[40px]">
-        {dates.map((date) => (
+        {data.map((item, index) => (
           <button
-            key={date}
-            onClick={() => setSelectedDate(date)}
+            key={index}
+            onClick={() => setSelectedDate(item)}
             className={`flex items-center justify-center w-12 h-12 rounded-full border-2 ${
-              selectedDate === date
+              selectedDate.date === item.date
                 ? "bg-[#092b4b] text-white"
                 : "border-blue-300 text-gray-600"
             }`}
-
-            style={{marginRight: "10px", padding: "30px"}}
+            style={{ marginRight: "10px", padding: "30px" }}
           >
-            {date}
+            {item.date}
           </button>
         ))}
       </div>
 
-      <div className="grid grid-cols-4 gap-4">
-        {timeSlots.map((slot) => (
-          <div key={slot.time} className="text-center border p-2 rounded-lg">
-            <div className="text-gray-600">{slot.time}</div>
-            <div className="text-green-500 font-bold">{slot.price}</div>
+      <div className="flex justify-center gap-4">
+        {selectedDate?.screenings.map((slot, index) => (
+          <div
+            key={index}
+            className="text-center border p-2 rounded-lg w-[25%]"
+          >
+            <div className="text-gray-600">
+              {getTimeFromDateTime(slot.startTime)}
+            </div>
+            <div className="text-green-500 font-bold">{`${slot.ticketPrice}.000đ`}</div>
           </div>
         ))}
       </div>
--- src/services/getDetailFilm.js
+++ src/services/getDetailFilm.js
@@ -1,14 +1,8 @@
 import * as httpRequest from "../utils/httpRequest";
 
 export const getDetailFilm = async (id) => {
-  console.log(id);
-  console.log(localStorage.getItem("accessToken"));
   try {
-    const res = await httpRequest.get(`/movies/${id}`, {
-      headers: {
-        Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
-      },
-    });
+    const res = await httpRequest.get(`movies/${id}`);
     return res.data;
   } catch (error) {
     console.log("Faied to get movie by Id", error);
