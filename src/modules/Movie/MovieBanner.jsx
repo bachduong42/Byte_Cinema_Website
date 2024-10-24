fc8ef304d8d7a48c9e827344ee6ee3304954c9c5
bachduong42 <bachduongvan0402@gmail.com>
2024-10-24 09:04:28 +0700

    fix ui

--- src/modules/Movie/MovieBanner.jsx
+++ src/modules/Movie/MovieBanner.jsx
@@ -9,7 +9,7 @@ function MovieBanner({ infor }) {
     };
     return (
         <div className="w-full h-screen relative ">
-            <Image src={infor.imagePaths[0]} alt="" className="w-full h-screen object-cover" />
+            <Image src={infor.imagePaths[1]} alt="" className="w-full h-screen object-cover" />
             <div className="absolute inset-0 bg-[#092B4B] opacity-80 z-40 flex flex-col justify-center items-start pl-[100px]">
                 <div className="text-[#0DB1F6] text-[40px] font-extrabold nunito-text">{infor.movieGenres[0].name}</div>
                 <div className="text-white text-[60px] anton-text">{infor.name}</div>
