d524ae54bb7df4bfad48d9468ba22dd07a3f2fa9
Nhung <hongnhung16052003@gmail.com>
2024-11-06 14:00:05 +0700

    fix: route of move schedules

--- src/config/routes.js
+++ src/config/routes.js
@@ -9,7 +9,7 @@ const routes = {
   theaterManagement: "/theater-management",
   showtimeManagement: "/showtime-management",
   detailedMovieList: "/movies/:type",
-  movieShchRedules: "/schedule-movie/:id",
+  movieSchedules: "/schedule-movie/:id",
   manageSchedules: "/manage-schedules",
   updateMovie: '/update-movie/:id',
   bookTicket: '/book-a-ticket'
